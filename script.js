7d02bd38168400ff9e222ef036d2242d35435c8b
Chris <106388029+chris-kirchner@users.noreply.github.com>
2023-04-11 13:05:27 -0700

    Add game logic with playRound() function

--- script.js
+++ script.js
@@ -9,8 +9,37 @@
 
 const rps = ["rock", "paper", "scissors"];
 
+let playerSelection = prompt("Choose rock, paper, or scissors").toLowerCase();
+
 function getComputerChoice() {
   return rps[Math.floor(Math.random() * 3)];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection !== "rock" || playerSelection !== "paper" || playerSelection !== "scissors") {
+    console.log("Input error. Did you spell something wrong, perhaps? Try again.");
+    alert("Input error. Did you spell something wrong, perhaps? Try again.");
+  }
+  else if (playerSelection === computerSelection) {
+    console.log("It's a tie!\nYou chose:", playerSelection, "Computer chose:", computerSelection);
+    alert("It's a tie!\r", "You chose:", playerSelection, "Computer chose:", computerSelection);
+  }
+  else if (playerSelection === "rock" && computerSelection === "scissors") {
+    console.log("You win!\nYou chose:", playerSelection, "Computer chose:", computerSelection);
+    alert("You win!\r", "You chose:", playerSelection, "Computer chose:", computerSelection);
+  } 
+  else if (playerSelection === "paper" && computerSelection === "rock") {
+    console.log("You win!\nYou chose:", playerSelection, "Computer chose:", computerSelection);
+    alert("You win!\r", "You chose:", playerSelection, "Computer chose:", computerSelection);
+  }
+  else if (playerSelection === "scissors" && computerSelection === "paper") {
+    console.log("You win!\nYou chose:", playerSelection, "Computer chose:", computerSelection);
+    alert("You win!\r", "You chose:", playerSelection, "Computer chose:", computerSelection);
+  }
+  else {
+    console.log("You lose!\nYou chose:", playerSelection, "Computer chose:", computerSelection);
+    alert("You lose!\r", "You chose:", playerSelection, "Computer chose:", computerSelection);
+  }
+}
+
+playRound(playerSelection, getComputerChoice());
