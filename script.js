516b3e063fbaba08f6eca0a62560e9af2d8c062a
Chris <106388029+chris-kirchner@users.noreply.github.com>
2023-04-11 14:59:56 -0700

    Update game logic to include input error check

--- script.js
+++ script.js
@@ -9,37 +9,44 @@
 
 const rps = ["rock", "paper", "scissors"];
 
-let playerSelection = prompt("Choose rock, paper, or scissors").toLowerCase();
+// let playerSelection = prompt("Choose rock, paper, or scissors").toLowerCase();
+
+function getPlayerChoice () {
+  return prompt("Choose rock, paper, or scissors").toLowerCase();
+}
 
 function getComputerChoice() {
   return rps[Math.floor(Math.random() * 3)];
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection !== "rock" || playerSelection !== "paper" || playerSelection !== "scissors") {
-    console.log("Input error. Did you spell something wrong, perhaps? Try again.");
-    alert("Input error. Did you spell something wrong, perhaps? Try again.");
+  const resultMsg = "\nYou chose: " + playerSelection + "\n" + "Computer chose: " + computerSelection;
+  const inputErr = "Input error. Try again."
+  
+  if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
+    console.log(inputErr);
+    alert(inputErr);
   }
   else if (playerSelection === computerSelection) {
-    console.log("It's a tie!\nYou chose:", playerSelection, "Computer chose:", computerSelection);
-    alert("It's a tie!\r", "You chose:", playerSelection, "Computer chose:", computerSelection);
+    console.log("It's a tie!" + resultMsg);
+    alert("It's a tie!" + resultMsg);
   }
   else if (playerSelection === "rock" && computerSelection === "scissors") {
-    console.log("You win!\nYou chose:", playerSelection, "Computer chose:", computerSelection);
-    alert("You win!\r", "You chose:", playerSelection, "Computer chose:", computerSelection);
+    console.log("You win!" + resultMsg);
+    alert("You win!" + resultMsg);
   } 
   else if (playerSelection === "paper" && computerSelection === "rock") {
-    console.log("You win!\nYou chose:", playerSelection, "Computer chose:", computerSelection);
-    alert("You win!\r", "You chose:", playerSelection, "Computer chose:", computerSelection);
+    console.log("You win!" + resultMsg);
+    alert("You win!" + resultMsg);
   }
   else if (playerSelection === "scissors" && computerSelection === "paper") {
-    console.log("You win!\nYou chose:", playerSelection, "Computer chose:", computerSelection);
-    alert("You win!\r", "You chose:", playerSelection, "Computer chose:", computerSelection);
+    console.log("You win!" + resultMsg);
+    alert("You win!" + resultMsg);
   }
   else {
-    console.log("You lose!\nYou chose:", playerSelection, "Computer chose:", computerSelection);
-    alert("You lose!\r", "You chose:", playerSelection, "Computer chose:", computerSelection);
+    console.log("You lose!" + resultMsg);
+    alert("You lose!" + resultMsg);
   }
 }
 
-playRound(playerSelection, getComputerChoice());
+playRound(getPlayerChoice(), getComputerChoice());
