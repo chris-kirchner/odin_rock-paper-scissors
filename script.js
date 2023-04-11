10a71fef97d15dccc6e141c7aaccc43d4d52b1ee
Chris <106388029+chris-kirchner@users.noreply.github.com>
2023-04-11 12:13:05 -0700

    Add html boilerplate and initial plan to script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello world");
+// User selects rock, paper, or scissors
+// CPU selects rock, paper, or scissors
+// check if user's selection beats CPU selection:
+//   if user choice === CPU choice: tie
+//   if rock: win if CPU chose scissors, lose if CPU chose paper
+//   if paper: win if CPU chose rock, lose if CPU chose scissors
+//   if scissors: win if CPU chose paper, lose if CPU chose rock
+// game reset
